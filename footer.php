39abe0ac045667f5f08f8e51c27fc5217aefd6c2
Raj180907 <rajshivraj2345@gmail.com>
2025-06-02 12:48:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center> BloodBridge 
+    -Bridging the gap between donors and recipients.<br>
+    (Blood Bank & Donation Management)<br>
+  Raj Shivraj Bhosale
+  <br>
+  
+  </center>
+  </div>
+
+
+</body>
+
+</html>
